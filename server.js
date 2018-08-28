a117b01289ae453293f44bd92a22d435ddfdc4f7
Katherine Smith <katherine.lo.smith@gmail.com>
2018-08-28 11:47:09 -0700

    Implement error handling

--- server.js
+++ server.js
@@ -7,9 +7,13 @@ let app = express();
 app.set('view engine', 'ejs');
 const PORT = process.env.PORT || 3000;
 
+
 const CONSTRING = process.env.DATABASE_URL || 'postgres://localhost:5432/books_app';
 let client = new pg.Client(CONSTRING);
 client.connect();
+client.on('error', err => {
+  console.error(err);
+});
 
 app.get('/', (request, response) => {
   response.render('index');
@@ -27,54 +31,13 @@ app.get('/books', (request, response) => {
       console.log(data);
       let bookData = data.rows;
       response.render('books', {books:bookData});
-
-    //   bookCountQuery();
+    })
+    .catch(err => {
+      console.log(err);
+      response.render('pages/error');
     });
 });
-
-// function bookCountQuery() {
-//   let SQL = "SELECT COUNT(*) FROM books";
-//   client.query(SQL)
-//     .then( data => {
-//       console.log(data);
-//       let countData = data.rows;
-//       response.render('books', {count:countData});
-//     });
-// }
-
-// app.get('/books', (request, response) => {
-//   let SQL = "SELECT COUNT(*) FROM books";
-//   client.query(SQL)
-//     .then( data => {
-//       console.log(data);
-//       let bookData = data.rows;
-//       response.render('books', {books:bookData});
-//     });
-// });
-
-// app.get('/categories', showCategories);
-// app.get('/links', showLinks);
-
-// function showCategories( request, response ) {
-//   let SQL = "SELECT * FROM categories";
-//   client.query(SQL)
-//     .then( data => {
-//       let categories = data.rows;
-//       response.render('categories', {items:categories});
-//     });
-// }
-
-// function showLinks( request, response ) {
-//   let SQL = "SELECT * FROM links";
-//   client.query(SQL)
-//     .then( data => {
-//       let listings = data.rows;
-//       console.log(listings);
-//       response.render('links', {items:listings});
-//     });
-// }
 
 app.use( express.static('./public') );
 
 app.listen( PORT, () => console.log('Server Up on ', PORT) );
-
